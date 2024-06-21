fff931c4b13e00975b85ea763b79f79d9fe7ed52
aslak <aslakrb@gmail.com>
2024-06-21 23:58:00 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<main>
+	{@render children()}
+</main>
+
+<style>
+	main {
+		font-family: Inter, Roboto, 'Helvetica Neue', 'Arial Nova', 'Nimbus Sans', Arial, sans-serif;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Title from './Title.svelte';
+</script>
+
+<Title>This is a headline without spans</Title>
